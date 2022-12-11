fe0c2b63aefa518ebef9499e6da710146c02ffcd
Manuel Jesús Romero Mateos <manu4.2012@gmail.com>
2022-12-11 11:39:02 +0100

    act

--- 20221207-serialbridge-uno/20221207-serialbridge-uno.ino
+++ 20221207-serialbridge-uno/20221207-serialbridge-uno.ino
@@ -3,6 +3,9 @@
 int led = 13; //Led connected to pin 13
 int data = 0; //Serial Data
 int data2 = 0; //RX TX Data
+String inputString;
+bool stringComplete;
+
 
 SoftwareSerial mySerial(6, 7); // RX, TX
 
@@ -16,19 +19,26 @@ void setup()
   pinMode(led, OUTPUT);
 }
 
-void loop()
-{
-  int query = 9;
- if (Serial.available() > 0) {
-    mySerial.write(Serial.parseInt());
-  }
 
+void loop()
+{  
+      mySerial.write(Serial.read());
+      
+/*
   if (mySerial.available() > 0) {
-    data2 = mySerial.read();
-    if(query == 9){
-       Serial.print("minuto ");
-          Serial.println(data2);
+    char inChar = mySerial.read();
+    if ((inChar != '\n'  && inChar != '\r' && inChar != '_')
+    && !(stringComplete)){
+      inputString += inChar;
+    }
+    
+    if (inChar == '_') {
+      stringComplete = true;
+    }
+    if (stringComplete){
+      Serial.println(inputString);
     }
   }
+  */
   delay(1000);
 }
