20878ec5ea2359b6e0780be68988c43d0d95095a
Manuel Jesús Romero Mateos <manu4.2012@gmail.com>
2022-12-11 22:38:49 +0100

    avances en el setup del uno

--- 20221207-serialbridge-uno/20221207-serialbridge-uno.ino
+++ 20221207-serialbridge-uno/20221207-serialbridge-uno.ino
@@ -1,9 +1,17 @@
 #include <SoftwareSerial.h>
 
+// VARIABLES COMUNES ENTRE UNO Y WEMOS
+int hora;
+int minuto;
+float temperatura;
+int isOnOff;
+int isIntruso;
+int isEmergencia;
+
 int led = 13; //Led connected to pin 13
 int data = 0; //Serial Data
 int data2 = 0; //RX TX Data
-String inputString;
+String recibo;
 bool stringComplete;
 
 
@@ -19,26 +27,62 @@ void setup()
   pinMode(led, OUTPUT);
 }
 
+// DIVIDIR STRINGS PARA TRANSFORMARLAS EN VARIABLES
+// fuente: https://arduino.stackexchange.com/a/1237
+String getValue(String data, char separator, int index){
+    int found = 0;
+    int strIndex[] = { 0, -1 };
+    int maxIndex = data.length() - 1;
 
-void loop()
-{  
-      mySerial.write(Serial.read());
-      
-/*
-  if (mySerial.available() > 0) {
-    char inChar = mySerial.read();
-    if ((inChar != '\n'  && inChar != '\r' && inChar != '_')
-    && !(stringComplete)){
-      inputString += inChar;
+    for (int i = 0; i <= maxIndex && found <= index; i++) {
+        if (data.charAt(i) == separator || i == maxIndex) {
+            found++;
+            strIndex[0] = strIndex[1] + 1;
+            strIndex[1] = (i == maxIndex) ? i+1 : i;
+        }
     }
-    
-    if (inChar == '_') {
-      stringComplete = true;
+    return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void wemos_recibir(){
+  if (mySerial.available() > 0) {
+    while (!stringComplete){
+      char inChar = mySerial.read();
+      if ((inChar != '\n'  && inChar != '\r' && inChar != '_')
+      && !(stringComplete)){
+        recibo += inChar;
+      }
+      
+      if (inChar == '_') {
+        stringComplete = true;
+      }
     }
     if (stringComplete){
-      Serial.println(inputString);
+      //hora = getValue(recibo, ':', 0).toInt();
+      //minuto = getValue(recibo, ':',1).toInt();
+      Serial.print("hora: ");
+      Serial.println(hora);
+      Serial.print("minuto: ");
+      Serial.println(minuto);
+      stringComplete = false;
+      recibo = "";
     }
+  } else {
+    Serial.println("[ERROR] Dispositivo serie del WEMOS no disponible");
   }
-  */
+}
+
+void wemos_enviar(){
+  if (mySerial.available() > 0) {
+    mySerial.write("14.5:1");
+    mySerial.write('_');
+  } else {
+    Serial.println("[ERROR] Dispositivo serie del WEMOS no disponible");
+  }
+}
+
+void loop(){
+  //wemos_recibir();
+  wemos_enviar();
   delay(1000);
 }
