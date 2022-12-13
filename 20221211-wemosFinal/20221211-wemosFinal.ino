2df1813a3a151cc32419ceed5de6e9aa4c9b8c7f
Manuel Jesús Romero Mateos <manu4.2012@gmail.com>
2022-12-13 12:57:24 +0100

    bruhh

--- 20221211-wemosFinal/20221211-wemosFinal.ino
+++ 20221211-wemosFinal/20221211-wemosFinal.ino
@@ -16,8 +16,8 @@ int isIntruso;
 int isEmergencia;
 
 // conectarse al wifi y ntc//
-const String ssid     = "manuwifi2";
-const String password = "l33tsupa";
+const String ssid     = "vodafone1B20";
+const String password = "NP7UTSVGTW5858";
 const long utcOffsetInSeconds = 3600;
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
@@ -32,10 +32,9 @@ int data = 0; //Serial Data
 int data2 = 0; //RX TX Data
 
 SoftwareSerial mySerial(D2, D1); // RX, TX
+WiFiServer server(80);
 
 void setup(){
-
-
   while (!Serial) {
     ; // Wait for serial port to connect. Needed for native USB port only
   }
@@ -45,9 +44,13 @@ void setup(){
   
   while ( WiFi.status() != WL_CONNECTED ) {
     delay ( 500 );
-    Serial.print ( "." );
+    //Serial.print ( "." );
   }
+  server.begin();
+  //Serial.println(F("Server started"));
 
+  // Print the IP address
+  //Serial.println(WiFi.localIP());
   timeClient.begin();
 }
 
@@ -69,30 +72,39 @@ String getValue(String data, char separator, int index){
 }
 
 float serialToFloat(String cadena, int pos){
-  Serial.print("cadena: ");
-  Serial.println(cadena);
-  Serial.print("trozo: ");
-  Serial.println(getValue(cadena, ';', 1));
+  //Serial.print("cadena: ");
+  //Serial.println(cadena);
+  //Serial.print("trozo: ");
+  //Serial.println(getValue(cadena, ';', 1));
   String temp = getValue(cadena, ';', pos);
   char temp2[8];
   temp.toCharArray(temp2,temp.length() + 1);
   return atof(temp2);
 }
 
+void actualizarHora(){
+  timeClient.update();
+  hora = timeClient.getHours();
+  minuto = timeClient.getMinutes();
+}
+
 void uno_enviar(){
 
   // CODIFICACIÓN DE LA HORA
-  timeClient.update();
   
-  char hora[7];
+  char horaf[7];
   char minu[7];
+  char onoff[7];
   
-  itoa(timeClient.getHours(),hora,10);
-  itoa(timeClient.getMinutes(),minu,10); 
+  itoa(hora,horaf,10);
+  itoa(minuto,minu,10); 
+  itoa(isOnOff,onoff,10);
   
-  mySerial.write(hora);
+  mySerial.write(horaf);
   mySerial.write(':');
   mySerial.write(minu);
+  mySerial.write(':');
+  mySerial.write(onoff);
 
   // CODIFICACIÓN DEL ESTADO DE LA BOMBILLA
 
@@ -112,8 +124,8 @@ void uno_recibir(){
       }
       
       if (inChar == '_') {
-        Serial.print("string que viene:");
-        Serial.println(recibo);
+        //Serial.print("string que viene:");
+        //Serial.println(recibo);
         stringComplete = true;
       }
     }
@@ -122,21 +134,101 @@ void uno_recibir(){
       isOnOff = getValue(recibo, ':',1).toInt();
       isIntruso = getValue(recibo, ':',2).toInt();
       isEmergencia = getValue(recibo, ':',3).toInt();
-      Serial.print("temperatura: ");
-      Serial.println(temperatura);
-      Serial.print("estado: ");
-      Serial.println(isOnOff);
-      Serial.print("intruso: ");
-      Serial.println(isIntruso);
-      Serial.print("emerg: ");
-      Serial.println(isEmergencia);
+      //Serial.print("temperatura: ");
+      //Serial.println(temperatura);
+      //Serial.print("estado: ");
+      //Serial.println(isOnOff);
+      //Serial.print("intruso: ");
+      //Serial.println(isIntruso);
+      //Serial.print("emerg: ");
+      //Serial.println(isEmergencia);
       stringComplete = false;
       recibo = "";
     }
   }
 }
 
+void web() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  //Serial.println(F("new client"));
+
+  client.setTimeout(5000); // default is 1000
+
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  //Serial.println(F("request: "));
+  //Serial.println(req);
+
+  // Match the request
+  int val;
+  if (req.indexOf(F("/gpio/0")) != -1) {
+    val = 0;
+  } else if (req.indexOf(F("/gpio/1")) != -1) {
+    val = 1;
+  } else {
+    //Serial.println(F("invalid request"));
+    val = digitalRead(LED_BUILTIN);
+  }
+
+  // Set LED according to the request
+  digitalWrite(LED_BUILTIN, val);
+
+  // read/ignore the rest of the request
+  // do not client.flush(): it is for output only, see below
+  while (client.available()) {
+    // byte by byte is not very efficient
+    client.read();
+  }
+  
+  // Send the response to the client
+  // it is OK for multiple small client.print/write,
+  // because nagle algorithm will group them into one single packet
+  client.print(F("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\n"));
+  client.print(F("<style> h1 { color:  #4f0ad5;  font-size: 1.5em; font-family: Arial, Helvetica, sans-serif; }  h2{ color:   #f44996   !important;  font-size: 1.4em !important; font-family: Arial, Helvetica, sans-serif; } p { font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; } button { border: none; border-radius: 200px; background-color:  #0262cf; color: #fbfbfb; transition: none !important; margin: 5 5px !important; padding: 5px;  transform: none !important; box-shadow: 1px 2px 4px 1px rgba(0, 0, 0, 0.3) !important;  } button:active { border: none; border-radius: 200px; transition: none !important; margin: 5 5px !important; padding: 5px;  box-shadow: none !important; transform: translate3d(1px, 1px, 1px) !important; }  .verde { background-color: #0b800b !important;} .amarillo { background-color: #c59100 !important;} .rojo { background-color: #e70f0f !important;} </style>\r\n"));
+  client.print(F("<head>\r\n"));
+  client.print(F("<meta http-equiv=\"refresh\" content=\"5\">\r\n"));
+  client.print(F("</head>\r\n"));\
+  client.print(F("<h1>WEMOS DEMO - "));
+  client.print(hora);
+  client.print(F(":"));
+  client.print(minuto);
+  client.print(F("</h1>\r\n"));
+  client.print(F("<p>Temperatura ahora mismo: "));
+  client.print(temperatura); 
+  client.print(F(" C</p>\r\n"));
+  client.print(F("<h2>Ha entrado alguien en la habitacion supersecreta? "));
+  if(isIntruso == 0){
+    client.print(F("NO"));
+  } else {
+    client.print(F("SI"));
+  }
+  client.print(F("</h2>\r\n"));  
+  if(isEmergencia == 0){
+    client.print(F("<button class=\"verde\">Toggle modo emergencia (ahora mismo: tranquilo"));
+  } else {
+    if(isEmergencia == 1){
+      client.print(F("<button class=\"amarillo\">Toggle modo emergencia (ahora mismo: moderado"));
+    } else {
+      client.print(F("<button class=\"rojo\">Toggle modo emergencia (ahora mismo: EMERGENCIA"));
+    }
+  }
+  client.print(F(")</button>\r\n"));
+  client.print(F("<button>Apagar/encender todo por completo</button>\r\n"));
+  client.print(F("</html>"));
+
+  // The client will actually be *flushed* then disconnected
+  // when the function returns and 'client' object is destroyed (out-of-scope)
+  // flush = ensure written data are received by the other side
+  //Serial.println(F("Disconnecting from client"));
+}
+
 void loop(){
+  actualizarHora();
+  web();
   uno_enviar();
   uno_recibir();
   delay(1000);
